132850063617a318433299c1019f710127d30dfc
nirma42 <90131728+nirma42@users.noreply.github.com>
2021-09-05 14:27:48 +0530

    Add files via upload
    First Commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of the following is incombustible?",
+    answer: "CCl4",
+    options: [
+      "H2",
+      "CCl4",
+      "C2H2",
+      "S"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Tumbling is the process of improving the __________ of the materials/parts.",
+    answer: "surface cleanlineses",
+    options: [
+      "surface finish",
+      "fatigue limit",
+      "creep limit",
+      "surface cleanlineses"
+    ]
+  },
+    {
+    numb: 3,
+    question: "__________ of air does not increase with increase in temperature.?",
+    answer: "Density",
+    options: [
+      "Density",
+      "Thermal diffusivity",
+      "Viscosity",
+      "Thermal conductivity"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Spherical shape of mercury droplets is due to its",
+    answer: "high surface tension.",
+    options: [
+      "high viscosity.",
+      "low surface tension.",
+      "high density.",
+      "high surface tension."
+    ]
+  },
+    {
+    numb: 5,
+    question: "The fluid property, due to which, mercury does not wet the glass is",
+    answer: "surface tension",
+    options: [
+      "surface tension",
+      "viscosity",
+      "cohesion",
+      "adhesion"
+    ]
+  },
+];
